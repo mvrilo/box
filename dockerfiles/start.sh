2ca1127ac637fe0c4f11514f051a4d1e995406bd
Murilo Santana <mvrilo@gmail.com>
2016-03-18 11:03:04 -0300

    missing headphones

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -4,7 +4,7 @@ set -x
 
 main() {
 	# build and run the containers
-	for c in base transmission couchpotato sickrage; do
+	for c in base transmission couchpotato sickrage headphones; do
 		docker build -t box/$c $c
 		[ "$c" == "base" ] && continue
 		$c/start.sh
