a6f1d34a90d9ce0c13fa1de93c6e3bf7855578b2
Murilo Santana <mvrilo@gmail.com>
2016-05-30 22:54:47 -0300

    s/\//-/, removing headphones from build

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -4,8 +4,8 @@ set -x
 
 main() {
 	# build and run the containers
-	for c in base transmission couchpotato sickrage headphones; do
-		docker build -t box/$c $c
+	for c in base transmission couchpotato sickrage; do
+		docker build -t box-$c $c
 		[ "$c" == "base" ] && continue
 		$c/start.sh
 	done
